ab65063859678cc57a354239d683f87026b917e1
MarcosWitcel <joao.marcos.witcel@gmail.com>
2022-02-12 16:08:57 -0300

    cadastro de movimento em andamento

--- src/main/java/com/marcoswitcel/restfulwebapi/controller/ProductMovementController.java
+++ src/main/java/com/marcoswitcel/restfulwebapi/controller/ProductMovementController.java
@@ -3,9 +3,8 @@ package com.marcoswitcel.restfulwebapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import com.marcoswitcel.restfulwebapi.model.ProductMovement;
 import com.marcoswitcel.restfulwebapi.repository.ProductMovementRepository;
@@ -26,4 +25,16 @@ public class ProductMovementController {
     public List<ProductMovement> getAllProductMovement() {
         return productMovementRepository.findAll();
     }
+
+    /**
+     * Ação de cadastro de o novo movimento de produto.
+     *
+     * @param productMovement Os dados da nova movimentação de produto que será cadastrada
+     * @return Retorna os dados da movimentação de produto agora com o ID caso der sucesso
+     */
+    @PostMapping("/product-movement")
+    public ProductMovement createProductMovement(@Validated @RequestBody ProductMovement productMovement) {
+        return productMovementRepository.save(productMovement);
+    }
+
 }
--- src/main/java/com/marcoswitcel/restfulwebapi/model/ProductMovement.java
+++ src/main/java/com/marcoswitcel/restfulwebapi/model/ProductMovement.java
@@ -6,6 +6,7 @@ import java.sql.Date;
 @Entity
 @Table(name = "product_movements")
 public class ProductMovement {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -25,4 +26,67 @@ public class ProductMovement {
 
     @Column(name  = "quantity")
     private long quantity;
+
+    public ProductMovement() {
+        super();
+    }
+
+    public ProductMovement(long id, Product product, int type, double sellPrice, Date sellDate, long quantity) {
+        super();
+        this.id = id;
+        this.product = product;
+        this.type = type;
+        this.sellPrice = sellPrice;
+        this.sellDate = sellDate;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public Date getSellDate() {
+        return sellDate;
+    }
+
+    public void setSellDate(Date sellDate) {
+        this.sellDate = sellDate;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
 }
