9e29d283ae7b5b3ade4ea3a47c4ff792336bbe96
MarcosWitcel <joao.marcos.witcel@gmail.com>
2022-02-12 18:46:18 -0300

    formatando

--- src/main/java/com/marcoswitcel/restfulwebapi/exception/ProductNotFoundException.java
+++ src/main/java/com/marcoswitcel/restfulwebapi/exception/ProductNotFoundException.java
@@ -5,4 +5,5 @@ public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
         super("Não pode encontrar o produto com id: " + id);
     }
+
 }
